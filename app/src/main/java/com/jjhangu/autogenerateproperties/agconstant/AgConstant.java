dfc4c5bdffc0b2aff75c682c0af85766ed84beae
jjhangu <jjhangu@gmail.com>
2016-01-29 16:31:16 +0900

    no message

--- app/src/main/java/com/jjhangu/autogenerateproperties/agconstant/AgConstant.java
+++ app/src/main/java/com/jjhangu/autogenerateproperties/agconstant/AgConstant.java
@@ -2,11 +2,11 @@ package com.jjhangu.autogenerateproperties.agconstant;
 
 public class AgConstant {
 /** config.properties **/
-public static String CONFIG_ID = "releaseId";
-public static String CONFIG_NAME = "release";
+public static final String CONFIG_ID = "releaseId";
+public static final String CONFIG_NAME = "release";
 
 /** secure.properties **/
-public static String SECURE_IP = "192.168.0.1";
-public static String SECURE_PORT = "443";
+public static final String SECURE_IP = "192.168.0.1";
+public static final String SECURE_PORT = "443";
 
 }
